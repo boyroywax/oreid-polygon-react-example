795c593d649795b99947526cf5351836a489406c
James <jmsdevln@gmail.com>
2022-06-23 14:22:11 -0700

    TST token balance added

--- src/OreIdWallet/UserBalance.tsx
+++ src/OreIdWallet/UserBalance.tsx
@@ -2,13 +2,14 @@ import { ChainNetwork, UserChainAccount, UserData } from "oreid-js";
 import { useUser } from "oreid-react";
 import React, { useEffect, useState } from "react";
 import { Button } from "src/Button";
-import { getBalance } from "src/helpers/polygon";
+import { getBalance, getTstTokenBalance } from "src/helpers/polygon";
 import { useOreId } from "oreid-react";
 
 
 export const UserBalance: React.FC = () => {
     const user: UserData | undefined = useUser()
     const[ userBalance, setUserBalance ] = useState("0.00")
+    const[ userTstBalance, setUserTstBalance ] = useState("0.00")
     const oreId = useOreId()
     const account = oreId.auth.user.data.chainAccounts.find(
         (chainAccount) => chainAccount.chainNetwork === ChainNetwork.PolygonMumbai
@@ -18,6 +19,9 @@ export const UserBalance: React.FC = () => {
     async function fetchBalance( account: UserChainAccount | undefined ) {
         const balance = await getBalance( account )
         setUserBalance( balance )
+
+        const tstBalance = await getTstTokenBalance( account )
+        setUserTstBalance( tstBalance )
     } 
 
 
@@ -36,6 +40,7 @@ export const UserBalance: React.FC = () => {
         <h2>{ account?.chainAccount }</h2>
         <h3>{ account?.chainNetwork }</h3>
         <h3><b><i>{ userBalance }</i></b> Test Tokens </h3>
+        <h3><b><i>{ userTstBalance }</i></b> Test Tokens </h3>
         </>
     )
 
--- src/helpers/polygon.ts
+++ src/helpers/polygon.ts
@@ -1,8 +1,35 @@
-import { UserChainAccount } from "oreid-js"
+import { User, UserChainAccount } from "oreid-js"
 import Web3 from "web3"
+import { AbiItem } from 'web3-utils';
+
+
+const provider = new Web3.providers.HttpProvider("https://rpc-mumbai.maticvigil.com")
+const web3 = new Web3( provider )
 
 export const getBalance = async ( account: UserChainAccount | undefined ): Promise<string> => {
-    const web3 = new Web3( new Web3.providers.HttpProvider("https://rpc-mumbai.maticvigil.com") )
     const balance = await web3.eth.getBalance( account?.chainAccount || "" ) 
-    return balance
+    const format = web3.utils.fromWei( balance )
+    return format
+}
+
+// The minimum ABI to get ERC20 Token balance
+const minABI: AbiItem = {    
+    constant: true,
+    inputs: [{ name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "balance", type: "uint256" }],
+    type: "function",
+}
+
+export const getTstTokenBalance = async ( account: UserChainAccount | undefined ): Promise<string> => {
+    const tokenAddress = "0x2d7882beDcbfDDce29Ba99965dd3cdF7fcB10A1e";
+    const walletAddress = account?.chainAccount
+
+    const contract = new web3.eth.Contract([minABI], tokenAddress);
+    const result = await contract.methods.balanceOf(walletAddress).call()
+
+    const format = web3.utils.fromWei(result)
+    console.log(format)
+    return format
+    
 }
